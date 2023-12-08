9750a72d50490aba75076a8927208ab8657d6803
lyndon.samson <lyndon.samson>
2023-12-08 23:26:18 +1100

    updates

--- _site/publish.sh
+++ _site/publish.sh
@@ -1,7 +1,7 @@
 export JEKYLL_VERSION=4.2.0
 docker run --rm --volume="$PWD:/srv/jekyll" -it jekyll/builder:$JEKYLL_VERSION jekyll build --trace
 read a
-git add _site _posts _config.yml
+git add _site _posts _config.yml assets
 git commit -m "updates"
 git push
 
